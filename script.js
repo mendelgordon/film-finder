5c2fd72ba5509e2427350b7ce59939ac297de01b
Mendel Gordon <mendelgordons@gmail.com>
2022-04-06 10:17:02 -0400

    Add more movies

--- script.js
+++ script.js
@@ -23,7 +23,8 @@ const getGenres = async () => {
 const getMovies = async () => {
    const selectedGenre = getSelectedGenre();
    const discoverMovieEndpoint = `/discover/movie`;
-   const requestParams = `?api_key=${tmdbKey}&with_genres=${selectedGenre}`;
+   const randomPage = Math.floor(Math.random() * 50) + 1;
+   const requestParams = `?api_key=${tmdbKey}&page=${randomPage}&with_genres=${selectedGenre}`;
    const urlToFetch = tmdbBaseUrl + discoverMovieEndpoint + requestParams;
    try {
       const response = await fetch(urlToFetch);
